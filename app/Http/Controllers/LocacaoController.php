b22d6d13ba498b090ff22b702591b78ae531026e
Henrique Ganz <henrique.ganz@gmail.com>
2023-12-22 14:40:04 -0300

    estudos de api e banco de dados

---
+++ app/Http/Controllers/LocacaoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\locacao;
+use Illuminate\Http\Request;
+
+class LocacaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\locacao  $locacao
+     * @return \Illuminate\Http\Response
+     */
+    public function show(locacao $locacao)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\locacao  $locacao
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(locacao $locacao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\locacao  $locacao
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, locacao $locacao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\locacao  $locacao
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(locacao $locacao)
+    {
+        //
+    }
+}
---
+++ database/factories/LocacaoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\locacao;
+use Faker\Generator as Faker;
+
+$factory->define(locacao::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
